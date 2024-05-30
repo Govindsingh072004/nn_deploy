230f15ac1c9d1e51b7aef9572fa7d1da24b9300a
Govind Singh <singhg81484@gmail.com>
2024-05-30 10:57:41 +0530

    Added shell file for preliminary_setup file

---
+++ preliminary_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install python3-venv
+sudo apt install python3-pip
+
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
+code .
+
+
+
+
+
+
