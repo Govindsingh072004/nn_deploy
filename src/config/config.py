de2bb83be6d8c6da76e935e8f9d369424f61dda6
Govind Singh <singhg81484@gmail.com>
2024-06-04 07:47:07 +0530

    Modified data_management.py and added preprocessors.py

--- src/config/config.py
+++ src/config/config.py
@@ -9,6 +9,10 @@ P = [NUM_INPUTS,2,1]
 # f_dict = dict(zip(range(0,5),["linear","sigmoid","tanh","relu","leaky relu"]))
 f = [None,'Linear','Sigmoid']
 
+
+
+
+
 LOSS_FUNCATION = "Mean Squared Error"
 MINI_BATCH_SIZE= 1
 
@@ -19,15 +23,21 @@ SAVED_MODEL_PATH=os.path.join(PACKAGE_ROOT,'trained_models')
 
 # theta0 = [None]
 # theta = [None]
+import os
+import pathlib
+import src
 
-# z = [None]*NUM_LAYERS
-# h = [None]*NUM_LAYERS
+NUM_INPUTS = 2
+NUM_LAYERS = 3
+P = [NUM_INPUTS,2,1]
 
-"""
-del_fl_by_del_z = [None]*NUM_LAYERS
-del_hl_by_del_theta0 = [None]*NUM_LAYERS
-del_hl_by_del_theta = [None]*NUM_LAYERS
-del_L_by_del_h = [None]*NUM_LAYERS
-del_L_by_del_theta0 = [None]*NUM_LAYERS
-del_L_by_del_theta = [None]*NUM_LAYERS
-"""
+f = [None,"linear","sigmoid"]
+
+LOSS_FUNCTION = "Mean Squared Error"
+MINI_BATCH_SIZE = 1
+
+PACKAGE_ROOT = pathlib.Path(src.__file__).resolve().parent
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets") 
+#"/src/datasets"
+SAVED_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
+#"/src/trained_models"
